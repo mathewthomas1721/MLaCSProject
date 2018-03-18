1ca3b471be92662c9dd99d4a2b9ee77b00eb1e7d
James Bannon <jjbannon@optonline.net>
2018-03-18 17:57:41 -0400

    fully functioning majority classifier

--- Code/Baseline_Models.py
+++ Code/Baseline_Models.py
@@ -17,29 +17,33 @@ from sklearn.dummy import DummyClassifier
 from sklearn.linear_model import LogisticRegression as LogisticModel
 
 DATA_PATH = "../Data/"
+WRITE_PATH= "../Data/OutData/"
 FIG_PATH = "../Figs/"
 POST_SEASON_ALL = DATA_PATH +"AtBats_PostSeason_2012-2017_update.csv"
 REG_SEASON_ALL = DATA_PATH +"AtBats_RegularSeason_2012-2017_update.csv"
 SPRING_TRN_ALL = DATA_PATH + "AtBats_SpringTraining_2012-2017_update.csv"
 
 
-def train_test_split(seas_df,t=.75):
-    num_train = int(np.round(t*seas_df.shape[0]))
-    num_test = int(seas_df.shape[0]-num_train)
+def train_test_split(seas_df,t=.75,mode="dummy"):
+   
+    if mode=="dummy":
+        num_train = int(np.round(t*seas_df.shape[0]))
+        num_test = int(seas_df.shape[0]-num_train)
     
-    print("number at bats: \t",end="")
-    print(seas_df.shape[0])
-    print("number training : \t",end="")
-    print(num_train)
-    print("number test: \t",end="")
-    print(num_test)
-    print("total is\t:" +str(num_train+num_test))
-    train = seas_df.head(num_train)
-    trn_x = train[
-    test = seas_df.tail(num_test)
-    print(train.tail())
-    print(test.head())
-    sys.exit(0)
+        train = seas_df.head(num_train)
+        test = seas_df.tail(num_test)
+
+    
+        trn_x = np.array(train['inning']).reshape((num_train,1))
+        trn_y = np.array(train['y'])
+    
+        tst_x = np.array(test['inning']).reshape((num_test,1))
+        tst_y = np.array(test['y'])
+        ## need to reshape so sklearn will work with it, otherwise throws a value error later
+        return trn_x,tst_x,trn_y,tst_y
+    else:
+         ##TODO: alter to return useful info for logistic regression, SVC
+        return 0
 
 """
 returns a data frame with season 
@@ -48,6 +52,20 @@ def season_subset(year):
     pass
 
 
+
+def display_results(Years,performance,name):
+    print("Year\t Score")
+    of = open(WRITE_PATH+name+".txt","w")
+    of.write("Year\t Score\n")
+    for j in range(len(Years)):
+        print(str(Years[j])+" \t "+ str(performance[j]))
+        of.write(str(Years[j])+" \t "+ str(performance[j]))
+    print("average score:" + str(np.mean(performance)))
+    of.write("Average Score:\t"+ str(np.mean(performance)))
+    of.write("\n")
+    of.write("Score is percentage correct")
+    of.close()
+        
 
 """
 Runs majority classifier
@@ -60,17 +78,21 @@ def Majority_Classifier(dfAtBats):
     for i in range(len(Years)):
         year = Years[i]
         curr_seas = dfAtBats.loc[dfAtBats['year']==year]
-        #print(curr_seas.head())
-        Train_X, Test_X,Train_y,Test_y = train_test_split(curr_seas)
-        plt.plot([year],[1+i],marker='o')
-    
+        X_train,X_test,y_train,y_test = train_test_split(curr_seas)
+        clf = DummyClassifier(strategy='most_frequent',random_state=0)
+        clf.fit(X_train,y_train)
+        score=clf.score(X_train,y_train)
+        plt.plot([year],[score],marker='o')
+        performance[i]=score
+    display_results(Years,performance,name="Majority_Classifier_Results")
     ax = plt.subplot(111)
-    ax.plot(x, y)
-
-    # Hide the right and top spines
     ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)   
-    plt.show()
+    ax.spines['top'].set_visible(False)
+    plt.title("Percent Correct by Majority Classifier By Year")
+    plt.xlabel("Year")
+    plt.ylabel("Percent Correct")
+    plt.savefig(FIG_PATH+"Majority_Classifier.pdf")
+    plt.close()
 
     
 """
@@ -78,6 +100,7 @@ def Majority_Classifier(dfAtBats):
 
 """
 def All_Dummy_Classifiers(AtBatSource):
+    strategies=["stratified","most_frequent","prior","uniform","constant"]
     pass
 
 
@@ -88,28 +111,38 @@ def main():
         # gives a column indexing season by year
         # binarizes the outcome of strikeouts to be 1 (yes strikeout) and 0 (not strikeout)
         #TODO: establish unique within-season game ID numbers so we can analyze 'first k games' etc. 
+             # will also make adding in pitchfx data easier
     
     
     dfRegSeason = pd.read_csv(REG_SEASON_ALL)
     dfRegSeason['y'] = np.where(dfRegSeason['descr']=='Strikeout', 1, 0)
     dfRegSeason['year'] = pd.DatetimeIndex(dfRegSeason['date']).year 
     
-    ## commented out for runtime
+    ## commented out for runtime, should be pre-processed
+
     #dfPostSeason = pd.read_csv(POST_SEASON_ALL)
     #dfSpringTrn = pd.read_csv(SPRING_TRN_ALL)
-
+    
 
 
     ### ---- Majority Classifier --- ###
-    #print(dfRegSeason.head(20))
+    
+    
     Majority_Classifier(dfRegSeason)
 
+
+    All_Dummy_Classifiers(dfRegSeason)
     ### --- Logistic Regression --- ###
-
-
-    ### --- Perceptron --- ###
     
-        #TODO: decide if we want to use this as a baseline or as a first model in the actual project
+    ##TODO: write this code
+
+
+    ### --- Perceptron --- ### 
+    #TODO: decide if we want to use this as a baseline or as a first model in the actual project
+    
+
+    ### --- Basic Support Vector Classifier --- ###
+    #TODO: decide if we want to use this as a baseline or as a first model in the actual project
 
 if __name__=="__main__":
     main()

473a0be119d6cb1b852986e5c8cd51db420d86ed
James Bannon <jjbannon@optonline.net>
2018-03-18 15:14:30 -0400

    progress on baseline models, added a directory for figures

--- Code/Baseline_Models.py
+++ Code/Baseline_Models.py
@@ -1,43 +1,68 @@
 """
-Python Code to 
+Python code to create a few baseline models for analyzing performance
 
 
+
+@Authors:
+    James Bannon
 
 """
 
 import numpy as np
 import pandas as pd
 import scipy as sp
+import matplotlib.pyplot as plt
+from sklearn.dummy import DummyClassifier
+from sklearn.linear_model import LogisticRegression as LogisticModel
 
 DATA_PATH = "../Data/"
+FIG_PATH = "../Figs/"
+POST_SEASON_ALL = DATA_PATH +"AtBats_PostSeason_2012-2017_update.csv"
+REG_SEASON_ALL = DATA_PATH +"AtBats_RegularSeason_2012-2017_update.csv"
+SPRING_TRN_ALL = DATA_PATH + "AtBats_SpringTraining_2012-2017_update.csv"
 
-def train_test_split():
+
+def train_test_split(seas_df):
     pass
 
 
-def season_subset
+"""
+returns a data frame with season 
+"""
+def season_subset(year):
+    pass
 
 
 
+"""
+Runs majority classifier
 
-def zero_one_loss(preds,res):
-    
+"""
 
+def Majority_Classifier(AtBatSource):
+    Years=np.arange(2012,2018)
+    performance = 
+    for year in Years:
+        df = 
 
+"""
+Tries dummy classifiers for all 
 
-
-
-
-
-
-
-
+"""
+def All_Dummy_Classifiers(AtBatSource):
+    pass
 
 
 
 def main():
-    ## Majority Classifier
+    ### ---- Majority Classifier --- ###
+    dfRegSeason = pd.read_csv(REG_SEASON_ALL)
+    print(dfRegSeason.head())
+    Years=np.arange(2012,2018)
+    print(Years)
 
 
+    ### --- Logistic Regression --- ###
+    
 if __name__=="__main__":
     main()

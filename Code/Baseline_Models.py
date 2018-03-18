8a8f02f482f89a63ae44696ea6972a563d21eb89
James Bannon <jjbannon@optonline.net>
2018-03-18 14:43:04 -0400

    more directories

---
+++ Code/Baseline_Models.py
@@ -0,0 +1,43 @@
+"""
+Python Code to 
+
+
+
+"""
+
+import numpy as np
+import pandas as pd
+import scipy as sp
+
+DATA_PATH = "../Data/"
+
+def train_test_split():
+    pass
+
+
+def season_subset
+
+
+
+
+def zero_one_loss(preds,res):
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    ## Majority Classifier
+
+
+if __name__=="__main__":
+    main()

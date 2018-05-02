ae7d51b4f56d9e1af5f45f6d75c12e73823bc422
mathewthomas <mathewthomas1721@gmail.com>
2018-05-02 18:29:20 -0400

    Random Forest update

--- Code/RandomForestRegressor.py
+++ Code/RandomForestRegressor.py
@@ -64,7 +64,7 @@ def RFRegressor(fname):
 
     # Saving feature names for later use
     feature_list = list(features.columns)
-
+    print(feature_list)
     # Convert to numpy array
     #features = np.array(features)
 
@@ -78,7 +78,7 @@ def RFRegressor(fname):
     print('Val Features Shape:', val_features.shape)
     print('Val Labels Shape:', val_labels.shape)
     '''
-    
+
     max_features = 21
     max_depth = 7
     min_samples_split =  2
@@ -93,8 +93,8 @@ def RFRegressor(fname):
         rf.fit(train_features, train_labels);
 
         # Use the forest's predict method on the test data
-        predictions = rf.predict(test_features)
-        print(compute_log_loss(test_labels,predictions), max_leaf_nodes)
+        predictions = rf.predict(val_features)
+        print(compute_log_loss(val_labels,predictions), max_leaf_nodes)
     '''
 
     train_features, test_features, train_labels, test_labels = train_test_split_season(features,validation = False)
@@ -115,139 +115,5 @@ def RFRegressor(fname):
     print(compute_log_loss(test_labels,predictions))
 
 
-    '''
-    # Pull out one tree from the forest
-    tree = rf.estimators_[5]
-
-    # Export the image to a dot file
-    export_graphviz(tree, out_file = "tree.dot", feature_names = feature_list, rounded = True, precision = 1)
-
-    # Use dot file to create a graph
-    (graph, ) = pydot.graph_from_dot_file("tree.dot")
-
-    # Write graph to a png file
-    graph.write_png('tree.png')
-
-    # Limit depth of tree to 3 levels
-    rf_small = RandomForestRegressor(n_estimators=10, max_depth = 3)
-    rf_small.fit(train_features, train_labels)
-
-    # Extract the small tree
-    tree_small = rf_small.estimators_[5]
-
-    # Save the tree as a png image
-    export_graphviz(tree_small, out_file = 'small_tree.dot', feature_names = feature_list, rounded = True, precision = 1)
-
-    (graph, ) = pydot.graph_from_dot_file('small_tree.dot')
-
-    graph.write_png('small_tree.png');
-
-    # Get numerical feature importances
-    importances = list(rf.feature_importances_)
-
-    # List of tuples with variable and importance
-    feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
-
-    # Sort the feature importances by most important first
-    feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
-
-    # Print out the feature and importances
-    [print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importances];
-
-    # New random forest with only the two most important variables
-    rf_most_important = RandomForestRegressor(n_estimators= 1000, random_state=42)
-
-    # Extract the two most important features
-    important_indices = [feature_list.index('temp_1'), feature_list.index('average')]
-    train_important = train_features[:, important_indices]
-    test_important = test_features[:, important_indices]
-
-    # Train the random forest
-    rf_most_important.fit(train_important, train_labels)
-
-    # Make predictions and determine the error
-    predictions = rf_most_important.predict(test_important)
-
-    errors = abs(predictions - test_labels)
-
-    # Display the performance metrics
-    print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
-
-    mape = np.mean(100 * (errors / test_labels))
-    accuracy = 100 - mape
-
-    print('Accuracy:', round(accuracy, 2), '%.')
-
-    # Set the style
-    plt.style.use('fivethirtyeight')
-
-    # list of x locations for plotting
-    x_values = list(range(len(importances)))
-
-    # Make a bar chart
-    plt.bar(x_values, importances, orientation = 'vertical')
-
-    # Tick labels for x axis
-    plt.xticks(x_values, feature_list, rotation='vertical')
-
-    # Axis labels and title
-    plt.ylabel('Importance'); plt.xlabel('Variable'); plt.title('Variable Importances');
-
-
-
-    # Dates of training values
-    months = features[:, feature_list.index('month')]
-    days = features[:, feature_list.index('day')]
-    years = features[:, feature_list.index('year')]
-
-    # List and then convert to datetime object
-    dates = [str(int(year)) + '-' + str(int(month)) + '-' + str(int(day)) for year, month, day in zip(years, months, days)]
-    dates = [datetime.datetime.strptime(date, '%Y-%m-%d') for date in dates]
-
-    # Dataframe with true values and dates
-    true_data = pd.DataFrame(data = {'date': dates, 'actual': labels})
-
-    # Dates of predictions
-    months = test_features[:, feature_list.index('month')]
-    days = test_features[:, feature_list.index('day')]
-    years = test_features[:, feature_list.index('year')]
-
-    # Column of dates
-    test_dates = [str(int(year)) + '-' + str(int(month)) + '-' + str(int(day)) for year, month, day in zip(years, months, days)]
-
-    # Convert to datetime objects
-    test_dates = [datetime.datetime.strptime(date, '%Y-%m-%d') for date in test_dates]
-
-    # Dataframe with predictions and dates
-    predictions_data = pd.DataFrame(data = {'date': test_dates, 'prediction': predictions})
-
-    # Plot the actual values
-    plt.plot(true_data['date'], true_data['actual'], 'b-', label = 'actual')
-
-    # Plot the predicted values
-    plt.plot(predictions_data['date'], predictions_data['prediction'], 'ro', label = 'prediction')
-    plt.xticks(rotation = '60');
-    plt.legend()
-
-    # Graph labels
-    plt.xlabel('Date'); plt.ylabel('Maximum Temperature (F)'); plt.title('Actual and Predicted Values');
-
-    # Make the data accessible for plotting
-    true_data['temp_1'] = features[:, feature_list.index('temp_1')]
-    true_data['average'] = features[:, feature_list.index('average')]
-    true_data['friend'] = features[:, feature_list.index('friend')]
-
-    # Plot all the data as lines
-    plt.plot(true_data['date'], true_data['actual'], 'b-', label  = 'actual', alpha = 1.0)
-    plt.plot(true_data['date'], true_data['temp_1'], 'y-', label  = 'temp_1', alpha = 1.0)
-    plt.plot(true_data['date'], true_data['average'], 'k-', label = 'average', alpha = 0.8)
-    plt.plot(true_data['date'], true_data['friend'], 'r-', label = 'friend', alpha = 0.3)
-
-    # Formatting plot
-    plt.legend(); plt.xticks(rotation = '60');
-
-    # Lables and title
-    plt.xlabel('Date'); plt.ylabel('Maximum Temperature (F)'); plt.title('Actual Max Temp and Variables');
-    '''
 
 RFRegressor('../Data/RegularSeasonFeatures2012.csv')
